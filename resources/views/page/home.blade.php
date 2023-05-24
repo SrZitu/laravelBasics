7accce456d408eb2c22b87e10433cf17b61bd126
SrZitu <zitu094@gmail.com>
2023-05-24 12:53:21 +0600

    conditional rendering using switch case

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -20,6 +20,23 @@
     @endif
     <h1>{{ $result }}</h1>
 
+    <h1>Result From Switch Statement</h1>
+    @switch($result)
+        @case(100)
+            <h1>Result is 100</h1>
+        @break
+
+        @case(1000)
+            <h1>Result is 1000</h1>
+        @break
+
+        @case(100000)
+            <h1>Result is 1 lakh</h1>
+        @break
+
+        @default
+            <h1>No match found</h1>
+    @endswitch
 </body>
 
 </html>
