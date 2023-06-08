84d1d7ffde0d05ed123f1f5d3510df10e4a02925
SrZitu <zitu094@gmail.com>
2023-06-08 11:53:50 +0600

    updated

--- app/Http/Controllers/CraftController.php
+++ app/Http/Controllers/CraftController.php
@@ -80,7 +80,8 @@ class CraftController extends Controller
         //     ->get();
         // return $result;
 
-        // advanced joining where we can apply conditon in joining
+
+        // ===============Advanced joining where we can apply conditon in joining=========
         // $result = DB::table('products')
         //     ->join('categories', function (JoinClause $join) {
         //         $join->on('products.category_id', '=', 'categories.id')
@@ -93,10 +94,11 @@ class CraftController extends Controller
         //     ->get();
         // return $result;
 
+
         /*                          ======Subquery Joins=====
-       You may use the joinSub, leftJoinSub, and rightJoinSub methods to join a query to a subquery.
-       Each of these methods receives three arguments: the subquery, its table alias, and a closure that defines the related columns.
-       */
+         You may use the joinSub, leftJoinSub, and rightJoinSub methods to join a query to a subquery.
+         Each of these methods receives three arguments: the subquery, its table alias, and a closure that defines the related columns.
+        */
         // $latestProduct = DB::table('products')
         //     ->select('products.id','products.title','products.remark','products.price')
         //     ->where('products.price', '=',2000)
@@ -109,6 +111,7 @@ class CraftController extends Controller
         // return $users;
 
 
+
         /*       ================Union===============
         The query builder also provides a convenient method to "union" two or more queries together.
          For example, you may create an initial query and use the union method to union it with more queries:
@@ -117,5 +120,160 @@ class CraftController extends Controller
         // $query2 = DB::table('products')->where('products.price', '=', 5000);
         // $result = $query1->union($query2)->get();
         // return $result;
+
+
+        /*       ================Where===============
+         The most basic call to the where method requires three arguments.
+         The first argument is the name of the column. The second argument is an operator,
+         which can be any of the database's supported operators.
+         The third argument is the value to compare against the column's value.
+         */
+
+        /* ===============  ->where()  ===============
+            ===============  ->where()   ===============
+         It works like and operation in which 2 where conditions needed to satisfy condity
+        If any of the where condition is false then return null
+
+         */
+        //  $data = DB::table('products')
+        //  ->where('price', '=', 2000)
+        //  ->where('title', 'LIKE', '%Slipper%')
+        //  ->get();
+        //  return $data;
+
+
+        /* ===============  ->where()  ===============
+            ===============  ->orwhere()   ===============*/
+
+        //Here if two conditions staisty seperately and two conditions satisfied result will be showed
+        //  $data = DB::table('products')
+        //  ->where('price', '=', 3000)
+        //  ->orwhere('title', 'LIKE', '%Slipper%')
+        //  ->get();
+        //  return $data;
+
+        /* ===============  ->whereNot()  ===============*/
+
+        //Exclude the where no column in database
+        //  $data = DB::table('products')
+        //  ->where('price', '=', 3000)
+        //  ->whereNOt('title', 'LIKE', '%Slipper%')
+        //  ->get();
+        //  return $data;
+
+        /*===============  ->orwhereNot()  ===============*
+            its like         ->where()
+                            ->orwhere()
+                                                         */
+        //  $data = DB::table('products')
+        //  ->where('price', '=', 3000)
+        //  ->orwhereNOt('title', 'LIKE', '%Shoe%')
+        //  ->get();
+        //  return $data;
+
+        /* ===============  ->whereBetween()  ===============*/
+
+        // $data = DB::table('products')
+        // ->whereBetween('price', [10, 1000])
+        // ->get();
+        // return $data;
+
+        /* ===============  ->whereNotBetween()  ===============*/
+
+        // $data = DB::table('products')
+        // ->whereNotBetween('price', [10, 2000])
+        // ->get();
+        // return $data;
+
+        /* ===============  -> whereBetweenColumns()  ===============
+       The whereBetweenColumns method verifies that a column's value is between the two values of two columns in the same table row:
+         */
+        // $patients = DB::table('patients')
+        //     ->whereBetweenColumns('weight', ['minimum_allowed_weight', 'maximum_allowed_weight'])
+        //     ->get();
+
+        /* ===============  -> whereIn()  ===============*/
+        // $data = DB::table('products')
+        //     ->whereIn('id', [1, 3, 10])
+        //     ->get();
+        // return $data;
+
+        /* ===============  -> whereNotIn()  ===============*/
+        // $data = DB::table('products')
+        //     ->whereNotIn('id', [1,2,4,6,8,10,12,14,16,18,20])
+        //     ->get();
+        // return $data;
+
+
+        /* ===============  -> whereNull()  ===============*/
+        // $data = DB::table('products')
+        // ->whereNull('price')
+        // ->get();
+        // return $data;
+
+        /* ===============  -> whereNull()  ===============*/
+        // $data = DB::table('products')
+        // ->whereNotNull('price')
+        // ->get();
+        // return $data;
+
+        /* ===============  -> whereDate()  ===============
+        The whereDate method may be used to compare a column's value against a date:
+        */
+        // $data = DB::table('products')
+        // ->whereDate('updated_at', '2023-08-05 ')
+        // ->get();
+        // return $data;
+
+        /* ===============  -> whereMonth()  ===============
+      The whereMonth method may be used to compare a column's value against a specific month:
+        */
+        // $data = DB::table('products')
+        // ->whereMonth('updated_at', '08 ')
+        // ->get();
+        // return $data;
+
+
+        /* ===============  -> whereDay()  ===============
+        The whereDay method may be used to compare a column's value against a specific Day:
+        */
+        // $data = DB::table('products')
+        // ->whereDay('updated_at', '05 ')
+        // ->get();
+        // return $data;
+
+        /* ===============  -> whereYear()  ===============
+        The whereYear method may be used to compare a column's value against a specific Year:
+        */
+        // $data = DB::table('products')
+        // ->whereYear('created_at', '2022')
+        // ->get();
+        // return $data;
+
+        /* ===============  -> whereTime()  ===============
+        The whereTime method may be used to compare a column's value against a specific Time:
+        */
+        // $data = DB::table('products')
+        // ->whereTime('created_at', '20:08:12')
+        // ->get();
+        // return $data;
+
+        /* ===============  -> whereColumn()  ===============
+         The whereColumn method may be used to verify that two columns are equal:
+        */
+        // $data = DB::table('products')
+        //     ->whereColumn('updated_at', '<', 'created_at')
+        //     ->get();
+        // return $data;
+
+        /* ===============  -> whereColumn()  ===============
+      You may also pass an array of column comparisons to the whereColumn method. These conditions will be joined using the and operator:
+        */
+        // $data = DB::table('products')
+        //     ->whereColumn([
+        //         ['category_id', '!=', 'brand_id'],
+        //         ['updated_at', '<', 'created_at'],
+        //     ])->get();
+        // return $data;
     }
 }
